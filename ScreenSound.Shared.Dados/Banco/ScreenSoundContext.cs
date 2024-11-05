9c8408dfe027aee8a6a3b5f168c3073f42d1e7d4
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-11-05 20:52:57 -0300

    Ajustes para o projeto da API funcionar com o banco

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -15,11 +15,15 @@ public class ScreenSoundContext: DbContext
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Server=localhost;Database=ScreenSoundV0;User ID=sa;Password=YourStrongPassword1!;TrustServerCertificate=True;";
 
     public ScreenSoundContext(DbContextOptions options) : base(options)
     {
 
+    }
+    public ScreenSoundContext()
+    {
+            
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
---
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContextFacotry.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using ScreenSound.Banco;
+
+public class ScreenSoundContextFactory : IDesignTimeDbContextFactory<ScreenSoundContext>
+{
+    public ScreenSoundContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ScreenSoundContext>();
+
+        optionsBuilder.UseSqlServer("Server=localhost;Database=ScreenSoundV0;User ID=sa;Password=YourStrongPassword1!;TrustServerCertificate=True;");
+
+        return new ScreenSoundContext(optionsBuilder.Options);
+    }
+}
