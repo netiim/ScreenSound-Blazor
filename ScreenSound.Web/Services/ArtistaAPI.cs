460b25dee7f3696bc01952cb9dc8773b90dc8dbc
Paulo Neto <31972103+netiim@users.noreply.github.com>
2024-11-05 14:48:53 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -0,0 +1,21 @@
+﻿using ScreenSound.API.Response;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistaAPI
+{
+    private readonly HttpClient _httpClient;
+
+    public ArtistaAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>?>("artistas");
+    }
+}
